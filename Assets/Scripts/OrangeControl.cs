27b8097997b4e73dfe2173ad1f8aff497b242068
SiTae9317 <gsvenger@gmail.com>
2022-04-02 22:55:25 +0900

    htkwak orange

--- Assets/Scripts/OrangeControl.cs
+++ Assets/Scripts/OrangeControl.cs
@@ -6,19 +6,30 @@ using VRC.Udon;
 
 public class OrangeControl : UdonSharpBehaviour
 {
+    public GameObject parentPrefab;
+
     void Start()
     {
         
     }
 
-    private void OnDrop()
+    private void OnPickup()
     {
         SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnGravityUse");
     }
 
+    private void OnDrop()
+    {
+    }
+
     public void OnGravityUse()
     {
-        Destroy(GetComponent<Rigidbody>());
-        //gameObject.GetComponent<Rigidbody>().useGravity = true;
+        GameObject newGo = VRCInstantiate(parentPrefab);
+
+        newGo.transform.position = gameObject.transform.position;
+        newGo.transform.rotation = gameObject.transform.rotation;
+        newGo.transform.localScale = gameObject.transform.localScale;
+
+        Destroy(gameObject);
     }
 }
