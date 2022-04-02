5c951bc6980b157d8648f8b8451d91a0fe460f8a
SiTae9317 <gsvenger@gmail.com>
2022-04-02 23:40:53 +0900

    htkwak scene

--- Assets/Scripts/ObjectControlTest.cs
+++ Assets/Scripts/ObjectControlTest.cs
@@ -6,37 +6,40 @@ using VRC.Udon;
 
 public class ObjectControlTest : UdonSharpBehaviour
 {
-    MeshRenderer mr;
-    private bool isTrigger;
-    public Material onMat;
-    public Material offMat;
+    //MeshRenderer mr;
+    //private bool isTrigger;
+    //public Material onMat;
+    //public Material offMat;
+    public OrangeControl oc;
 
     void Start()
     {
-        mr = gameObject.GetComponent<MeshRenderer>();
-        materialAction(isTrigger);
+        //mr = gameObject.GetComponent<MeshRenderer>();
+        //materialAction(isTrigger);
     }
 
     public void Interact()
     {
-        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "MyButtonClickEvent");
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnUnlock");
     }
 
-    public void MyButtonClickEvent()
+    public void OnUnlock()
     {
-        isTrigger = !isTrigger;
-        materialAction(isTrigger);
+        oc.OnUnlock();
+        Destroy(gameObject);
+        //isTrigger = !isTrigger;
+        //materialAction(isTrigger);
     }
 
-    void materialAction(bool curStatus)
-    {
-        if (curStatus)
-        {
-            mr.sharedMaterial = onMat;
-        }
-        else
-        {
-            mr.sharedMaterial = offMat;
-        }
-    }
+    //void materialAction(bool curStatus)
+    //{
+    //    if (curStatus)
+    //    {
+    //        mr.sharedMaterial = onMat;
+    //    }
+    //    else
+    //    {
+    //        mr.sharedMaterial = offMat;
+    //    }
+    //}
 }
--- Assets/Scripts/OrangeControl.cs
+++ Assets/Scripts/OrangeControl.cs
@@ -7,29 +7,49 @@ using VRC.Udon;
 public class OrangeControl : UdonSharpBehaviour
 {
     public GameObject parentPrefab;
+    private bool isInit = false;
+
+    private Vector3 keepPosition;
+    private Quaternion keepRotation;
 
     void Start()
     {
-        
+        keepPosition = gameObject.transform.position;
+        keepRotation = gameObject.transform.rotation;
     }
 
-    private void OnPickup()
+    private void Update()
     {
-        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnGravityUse");
+        if(!isInit)
+        {
+            gameObject.transform.position = keepPosition;
+            gameObject.transform.rotation = keepRotation;
+        }
     }
 
-    private void OnDrop()
+    //private void OnPickup()
+    //{
+    //    SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnGravityUse");
+    //}
+
+    //private void OnDrop()
+    //{
+    //}
+
+    //public void OnGravityUse()
+    //{
+    //    GameObject newGo = VRCInstantiate(parentPrefab);
+
+    //    newGo.transform.position = gameObject.transform.position;
+    //    newGo.transform.rotation = gameObject.transform.rotation;
+    //    newGo.transform.localScale = gameObject.transform.localScale;
+
+    //    Destroy(gameObject);
+    //}
+
+    public void OnUnlock()
     {
-    }
-
-    public void OnGravityUse()
-    {
-        GameObject newGo = VRCInstantiate(parentPrefab);
-
-        newGo.transform.position = gameObject.transform.position;
-        newGo.transform.rotation = gameObject.transform.rotation;
-        newGo.transform.localScale = gameObject.transform.localScale;
-
-        Destroy(gameObject);
+        isInit = true;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 }
