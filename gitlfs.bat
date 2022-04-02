6b1c76a905716bb1a0642c8afa57922394f869f9
KimUSeok <100741396+KimUSeok@users.noreply.github.com>
2022-04-03 00:01:39 +0900

    lfs upload

--- gitlfs.bat
+++ gitlfs.bat
@@ -1,5 +1,4 @@
-git lfs track "Useok_Scene_26_Combine.unity"
-git lfs track "Useok_Scene_27_Combine.unity"
+git lfs track "Useok_Scene_28_Combine.unity"
 git add .
 git commit -m "lfs upload"
 git push
